3431a44b9e211cf1aa429f3c61f4b158ddd380cf
Miguel Rodriguez <mjrodrigueza@ufpso.edu.co>
2023-11-29 15:51:32 -0500

    TaskList

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex justify-between px-4 py-1 mt-1 rounded bg-gray-200">
+      tarea 1
+      <span class="transition-colors duration-300 hover:bg-red-700 hover:cursor-pointer px-2 text-center text-white bg-red-600 rounded">X</span>
+    </div>
+</template>
